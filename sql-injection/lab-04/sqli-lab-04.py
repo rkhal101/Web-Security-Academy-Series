1bbb09d75d03e45e5284a5bfcc20ce654fb7de84
Rana Khalil <ranakhalil101@gmail.com>
2022-11-09 10:26:37 -0500

    Update sqli-lab-04.py

--- sql-injection/lab-04/sqli-lab-04.py
+++ sql-injection/lab-04/sqli-lab-04.py
@@ -3,7 +3,7 @@ import sys
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-proxies = {'http': 'http://127.0.0.1:8080', 'https': 'https://127.0.0.1:8080'}
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
 def exploit_sqli_column_number(url):
     path = "filter?category=Gifts"
