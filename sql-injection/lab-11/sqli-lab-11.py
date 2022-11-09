82f758396bb2e2de001c9966031ce54949058073
Rana Khalil <ranakhalil101@gmail.com>
2022-11-09 10:28:41 -0500

    Update sqli-lab-11.py

--- sql-injection/lab-11/sqli-lab-11.py
+++ sql-injection/lab-11/sqli-lab-11.py
@@ -5,7 +5,7 @@ import urllib
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-proxies = {'http': 'http://127.0.0.1:8080', 'https': 'https://127.0.0.1:8080'}
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
 def sqli_password(url):
     password_extracted = ""
