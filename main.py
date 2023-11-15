979d78aaf71124f27fde23a94b57c0e75ac2d042
Chris <atnl@protonmail.com>
2023-11-14 22:55:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,8 @@ class Request:
             print(e)
 
     # The payload will be appended to the end of URL.
-    def exploit(self, url, payload):
+    @staticmethod
+    def exploit(url, payload):
         """Exploit function that accepts a URL and a payload as arguments."""
         try:
             response = requests.post(url, verify=False, proxies=proxies, data=payload)
@@ -64,21 +65,25 @@ class Request:
 # main function
 def main(name):
     """Main function. Parses the command line arguments and executes the appropriate function."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-u', '--url', required=True, help='URL to be exploited')
-    parser.add_argument('-p', '--payload', required=True, help='Payload to be appended to the URL')
-    parser.add_argument('-m', '--method', required=True, help='Method to be used for the request')
-    args = parser.parse_args()
-    url = args.url
-    payload = args.payload
-    method = args.method
-    Request(url, method).exploit(url, payload)
-    Request(url, method).get_request()
-    Request(url, method).post_request()
-    Request(url, method).exploit(url, payload)
-    Request(url, method).get_request()
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-u', '--url', required=True, help='URL to be exploited')
+        parser.add_argument('-p', '--payload', required=True, help='Payload to be appended to the URL')
+        parser.add_argument('-m', '--method', required=True, help='Method to be used for the request')
+        args = parser.parse_args()
+        url = args.url
+        payload = args.payload
+        method = args.method
+        Request(url, method).exploit(url, payload)
+        Request(url, method).get_request()
+        Request(url, method).post_request()
+        Request(url, method).exploit(url, payload)
+        Request(url, method).get_request()
+    except IndexError:
+        print(f'Usage: {name} -u <url> -p <payload> -m <method>')
+        sys.exit(1)
 
 
 # guarding the name variable
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv)
